d19232d989d9a3f631a05394b948c4a2e9e6a121
carlososm <carlos.olivares.sm@gmail.com>
2017-09-22 15:30:05 +0200

    middleware

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ mongoose.connect('mongodb://localhost/Tododb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res){
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
 
 var routes = require('./api/routes/smartWasteRoutes'); //importing route
 routes(app); //register the route
