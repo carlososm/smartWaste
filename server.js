46264e09dd8ab6defbe33eb19510a202c94ff40a
carlososm <carlos.olivares.sm@gmail.com>
2017-09-26 19:15:06 +0200

    server on 8080

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  Trade = require('./api/models/smartWasteModel'), //created model loading here
-  bodyParser = require('body-parser');
+var express = require('express'), app = express(),
+    port = process.env.PORT || 8080, mongoose = require('mongoose'),
+    Trade =
+        require('./api/models/smartWasteModel'), // created model loading here
+    bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tododb');
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-app.use(function(req, res){
-  res.status(404).send({url: req.originalUrl + ' not found'})
+app.use(function(req, res) {
+  res.status(404).send({url : req.originalUrl + ' not found'})
 });
 
-
-var routes = require('./api/routes/smartWasteRoutes'); //importing route
-routes(app); //register the route
-
+var routes = require('./api/routes/smartWasteRoutes'); // importing route
+routes(app);                                           // register the route
 
 app.listen(port);
-
 
 console.log('smart waste RESTful API server started on: ' + port);
