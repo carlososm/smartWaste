3df92318da76872aa26f573009d43d097e4cc71f
carlososm <carlos.olivares.sm@gmail.com>
2017-09-28 17:23:29 +0200

    bug

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,12 @@ angular.module('angularSmartWaste', []).config([
   '$routeProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider
-        .when('/trades', {
-          templateUrl : '/partials/trades.html',
-          controller : mainController
-        })
+        .when(
+            '/trades',
+            {templateUrl : 'partials/trades.html', controller : mainController})
         .when(
             '/trades/:tradeId',
-            {templateUrl : '/partials/trade.html', controller : mainController})
+            {templateUrl : 'partials/trade.html', controller : mainController})
         .otherwise({redirectTo : '/trades'});
 
     $locationProvider.html5Mode(true);
