7f7b6e4ee862772483930b3bccc3b64c3d22fc97
carlososm <carlos.olivares.sm@gmail.com>
2017-09-29 14:27:39 +0200

    bug

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,36 @@
-angular.module('angularSmartWaste', [ 'ngRoute' ]).config(function($routeProvider, $locationProvider) {
-  $routeProvider
-      .when('/contId',
-            {templateUrl : 'partials/home.html', controller : mainController})
-      .when('/new',
-            {templateUrl : 'partials/new.html', controller : newController})
-      .when('/contId/trades', {
-        templateUrl : 'partials/trades.html',
-        controller : mTradesController
-      })
-      .when('/contId/trades/:tradeId',
-            {templateUrl : 'partials/trade.html', controller : tradeController})
-      .when('/contId/newtrade', {
-        templateUrl : 'partials/newtrade.html',
-        controller : mTradesController
-      })
-      .when('/contId/accounts', {
-        templateUrl : 'partials/accounts.html',
-        controller : mAccountsController
-      })
-      .when('/contId/accounts/:accountId', {
-        templateUrl : 'partials/account.html',
-        controller : accountController
-      })
-      .when('/contId/newaccount', {
-        templateUrl : 'partials/newaccount.html',
-        controller : mAccountsController
-      })
-      .otherwise({redirectTo : '/contId'});
+angular.module('angularSmartWaste', [ 'ngRoute' ])
+    .config(function($routeProvider, $locationProvider) {
+      $routeProvider
+          .when(
+              '/contId',
+              {templateUrl : 'partials/home.html', controller : mainController})
+          .when('/new',
+                {templateUrl : 'partials/new.html', controller : newController})
+          .when('/contId/trades', {
+            templateUrl : 'partials/trades.html',
+            controller : mTradesController
+          })
+          .when('/contId/trades/:tradeId', {
+            templateUrl : 'partials/trade.html',
+            controller : tradeController
+          })
+          .when('/contId/newtrade', {
+            templateUrl : 'partials/newtrade.html',
+            controller : mTradesController
+          })
+          .when('/contId/accounts', {
+            templateUrl : 'partials/accounts.html',
+            controller : mAccountsController
+          })
+          .when('/contId/accounts/:accountId', {
+            templateUrl : 'partials/account.html',
+            controller : accountController
+          })
+          .when('/contId/newaccount', {
+            templateUrl : 'partials/newaccount.html',
+            controller : mAccountsController
+          })
+          .otherwise({redirectTo : '/contId'});
 
-  $locationProvider.html5Mode(true);
-  }
-]);
+      $locationProvider.html5Mode(true);
+    });
