fe7ae065fee354d6432c3ea2b0ce094aefa66893
carlososm <carlos.olivares.sm@gmail.com>
2017-09-29 14:13:01 +0200

    filter handler

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -23,6 +23,36 @@ function mainController($scope, $http, $routeParams) {
               });
     $("body").toggleClass("no-scroll");
   });
+  $("#activity-filter-btn").click(function() {
+    $("#activity-filter").toggleClass("filter-hidden");
+    $("#activity-filter").removeClass("hidden");
+    $("#account-filter").addClass("filter-hidden");
+    $("#account-filter").addClass("hidden");
+    $("#activity-filter")
+        .bind("animationend webkitAnimationnd oAnimationEnd MSAnimationEnd",
+              function() {
+                if ($(this).hasClass("filter-hidden")) {
+                  $(this).addClass("hidden");
+                } else {
+                  $(this).removeClass("hidden")
+                }
+              });
+    $("body").toggleClass("no-scroll");
+  });
+
+  $(".filter .fa-times").click(function() {
+    $(this).parent().parent().addClass("filter-hidden");
+    $(this).parent().parent().bind(
+        "animationend webkitAnimationnd oAnimationEnd MSAnimationEnd",
+        function() {
+          if ($(this).hasClass("filter-hidden")) {
+            $(this).addClass("hidden");
+          } else {
+            $(this).removeClass("hidden")
+          }
+        });
+    $("body").toggleClass("no-scroll");
+  });
 }
 
 function newController($scope, $http) {}
