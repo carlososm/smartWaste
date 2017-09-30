ae2124a7c43dffa99a82a75a71b94b36ceec3854
carlososm <carlos.olivares.sm@gmail.com>
2017-09-30 16:58:03 +0200

    bug

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://localhost/smartWaste');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-require('./api/routes')(app);
+require('./api/routes/')(app);
 // var routes = require('./api/routes/smartWasteRoutes'); // importing route
 // routes(app);
 
