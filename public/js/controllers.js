f711a9854eaa2c53dea51940c598fc21d083a1c4
carlososm <carlos.olivares.sm@gmail.com>
2017-09-30 15:34:07 +0200

    test

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,126 +8,116 @@ function mainController($scope, $http, $routeParams) {
       })
       .error(function(data) { console.log('Error 3:' + data); });
 
+  /** sign in control **/
+  $("#signin-intro").click(function() {
+    $("#signin-div").toggleClass("hidden");
+  });
+
+  /** filter control **/
+  $("#account-filter-btn").click(function() {
+    $("#activity-filter").toggleClass("filter-hidden");
+    $("#activity-filter").removeClass("hidden");
+    $("#account-filter").addClass("filter-hidden");
+    $("#account-filter").addClass("hidden");
+    $("#activity-filter")
+        .bind("animationend webkitAnimationnd oAnimationEnd MSAnimationEnd",
+              function() {
+                if ($(this).hasClass("filter-hidden")) {
+                  $(this).addClass("hidden");
+                } else {
+                  $(this).removeClass("hidden")
+                }
+              });
+    $("body").toggleClass("no-scroll");
+  });
+  $("#activity-filter-btn").click(function() {
+    $("#activity-filter").toggleClass("filter-hidden");
+    $("#activity-filter").removeClass("hidden");
+    $("#account-filter").addClass("filter-hidden");
+    $("#account-filter").addClass("hidden");
+    $("#activity-filter")
+        .bind("animationend webkitAnimationnd oAnimationEnd MSAnimationEnd",
+              function() {
+                if ($(this).hasClass("filter-hidden")) {
+                  $(this).addClass("hidden");
+                } else {
+                  $(this).removeClass("hidden")
+                }
+              });
+    $("body").toggleClass("no-scroll");
+  });
+
+  $(".filter .fa-times").click(function() {
+    $(this).parent().parent().addClass("filter-hidden");
+    $(this).parent().parent().bind(
+        "animationend webkitAnimationnd oAnimationEnd MSAnimationEnd",
+        function() {
+          if ($(this).hasClass("filter-hidden")) {
+            $(this).addClass("hidden");
+          } else {
+            $(this).removeClass("hidden")
+          }
+        });
+    $("body").toggleClass("no-scroll");
+  });
+}
+
+function newController($scope, $http) {}
+
+function mTradesController($scope, $http) {
+  $scope.formData = {};
+
+  // Cuando se cargue la página, pide del API trades los trades
+  $http.get('/trades')
+      .success(function(data) {
+        $scope.trades = data;
+        console.log(data)
+      })
+      .error(function(data) { console.log('Error 1: ' + data); });
+
+  // Cuando se añade un nuevo TRADE, manda el texto a la API
+  $scope.createTrade = function() {
+    $http.post('/trades', $scope.formData)
+        .success(function(data) {
+          $scope.formData = {};
+          $scope.trades = data;
+          console.log(data);
+        })
+        .error(function(data) { console.log('Error 2:' + data); });
+  };
+}
+
+function tradeController($scope, $http, $routeParams) {
+  var tradeId = $routeParams.tradeId;
+  // // Muestra un TRADE despues de checkearlo como acabado
+  $http.get('/trades/' + tradeId)
+      .success(function(data) {
+        $scope.trades = data;
+        console.log(data);
+      })
+      .error(function(data) { console.log('Error 3:' + data); });
+
   // Borra un TRADE despues de checkearlo como acabado
-  $scope.deleteTrade =
-      function(id) {
+  $scope.deleteTrade = function(id) {
     $http.delete('/trades/' + id)
         .success(function(data) {
           $scope.trades = data;
           console.log(data);
         })
         .error(function(data) { console.log('Error 4:' + data); });
+  };
 
-    /** sign in control **/
-    $("#signin-intro").click(function() {
-      $("#signin-div").toggleClass("hidden");
-    });
-
-    /** filter control **/
-    $("#account-filter-btn").click(function() {
-      $("#activity-filter").toggleClass("filter-hidden");
-      $("#activity-filter").removeClass("hidden");
-      $("#account-filter").addClass("filter-hidden");
-      $("#account-filter").addClass("hidden");
-      $("#activity-filter")
-          .bind("animationend webkitAnimationnd oAnimationEnd MSAnimationEnd",
-                function() {
-                  if ($(this).hasClass("filter-hidden")) {
-                    $(this).addClass("hidden");
-                  } else {
-                    $(this).removeClass("hidden")
-                  }
-                });
-      $("body").toggleClass("no-scroll");
-    });
-    $("#activity-filter-btn").click(function() {
-      $("#activity-filter").toggleClass("filter-hidden");
-      $("#activity-filter").removeClass("hidden");
-      $("#account-filter").addClass("filter-hidden");
-      $("#account-filter").addClass("hidden");
-      $("#activity-filter")
-          .bind("animationend webkitAnimationnd oAnimationEnd MSAnimationEnd",
-                function() {
-                  if ($(this).hasClass("filter-hidden")) {
-                    $(this).addClass("hidden");
-                  } else {
-                    $(this).removeClass("hidden")
-                  }
-                });
-      $("body").toggleClass("no-scroll");
-    });
-
-    $(".filter .fa-times").click(function() {
-      $(this).parent().parent().addClass("filter-hidden");
-      $(this).parent().parent().bind(
-          "animationend webkitAnimationnd oAnimationEnd MSAnimationEnd",
-          function() {
-            if ($(this).hasClass("filter-hidden")) {
-              $(this).addClass("hidden");
-            } else {
-              $(this).removeClass("hidden")
-            }
-          });
-      $("body").toggleClass("no-scroll");
-    });
-  }
-
-  function newController($scope, $http) {}
-
-  function mTradesController($scope, $http) {
-    $scope.formData = {};
-
-    // Cuando se cargue la página, pide del API trades los trades
-    $http.get('/trades')
-        .success(function(data) {
-          $scope.trades = data;
-          console.log(data)
-        })
-        .error(function(data) { console.log('Error 1: ' + data); });
-
-    // Cuando se añade un nuevo TRADE, manda el texto a la API
-    $scope.createTrade = function() {
-      $http.post('/trades', $scope.formData)
-          .success(function(data) {
-            $scope.formData = {};
-            $scope.trades = data;
-            console.log(data);
-          })
-          .error(function(data) { console.log('Error 2:' + data); });
-    };
-  }
-
-  function tradeController($scope, $http, $routeParams) {
-    var tradeId = $routeParams.tradeId;
-    // // Muestra un TRADE despues de checkearlo como acabado
-    $http.get('/trades/' + tradeId)
+  // Edita un TRADE despues de checkearlo como acabado
+  $scope.updateTrade = function(id) {
+    $http.update('/trades/' + id, $scope.trades)
         .success(function(data) {
           $scope.trades = data;
           console.log(data);
         })
-        .error(function(data) { console.log('Error 3:' + data); });
+        .error(function(data) { console.log('Error 4:' + data); });
+  };
+}
 
-    // Borra un TRADE despues de checkearlo como acabado
-    $scope.deleteTrade = function(id) {
-      $http.delete('/trades/' + id)
-          .success(function(data) {
-            $scope.trades = data;
-            console.log(data);
-          })
-          .error(function(data) { console.log('Error 4:' + data); });
-    };
+function mAccountsController($scope, $http) {}
 
-    // Edita un TRADE despues de checkearlo como acabado
-    $scope.updateTrade = function(id) {
-      $http.update('/trades/' + id, $scope.trades)
-          .success(function(data) {
-            $scope.trades = data;
-            console.log(data);
-          })
-          .error(function(data) { console.log('Error 4:' + data); });
-    };
-  }
-
-  function mAccountsController($scope, $http) {}
-
-  function accountController($scope, $http) {}
+function accountController($scope, $http) {}
