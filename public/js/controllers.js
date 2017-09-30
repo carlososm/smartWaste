3d96d2e5773221e390a59f1eb7c96aad4f646891
carlososm <carlos.olivares.sm@gmail.com>
2017-09-30 16:16:19 +0200

    bug

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,8 +6,8 @@ app.run([
   }
 ]);
 
-function mainController($scope, $routeScope, $http, $routeParams) {
-  var contId = $scope.contId;
+function mainController($scope, $rootScope, $http, $routeParams) {
+  var contId = $scop.contId;
   $rootScope.contId = contId;
   // // Muestra un CONTABILITY despues de checkearlo como acabado
   $http.get('/' + contId)
